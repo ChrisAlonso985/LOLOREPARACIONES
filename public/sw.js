d499f80b02617b1849f05ac0d72631afc72da616
ChrisAlonso985 <christianalonso405@gmail.com>
2026-09-24 16:22:17 -0300

    Agregar una consulta gratis antes del pago

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lolo-real-v35';
+const CACHE='lolo-real-v37';
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg'])));
