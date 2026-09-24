bde7183ef353d79e1df88c349cc7166272e98fab
ChrisAlonso985 <christianalonso405@gmail.com>
2026-09-24 18:38:00 -0300

    Actualizar cache para nuevo logo LOLO

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE='lolo-real-v44';
+const CACHE='lolo-real-v45';
 self.addEventListener('install',e=>{
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg'])));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg','/logo-lolo.svg'])));
 });
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
