e7ce21fa9d5af832cd014d170701e5dafdddb673
ChrisAlonso985 <christianalonso405@gmail.com>
2026-09-24 17:04:12 -0300

    Agregar autor e instalación de LOLO en celular

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lolo-real-v37';
+const CACHE='lolo-real-v38';
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg'])));
