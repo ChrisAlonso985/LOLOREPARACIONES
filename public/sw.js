9c1ec437c93b1802811649a102e3402699d2c704
ChrisAlonso985 <christianalonso405@gmail.com>
2026-09-24 17:59:23 -0300

    Bump PWA cache for Vision IA update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lolo-real-v41';
+const CACHE='lolo-real-v42';
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg'])));
