3f51c09c8cdc9023466a5056341e13e3911d698b
ChrisAlonso985 <christianalonso405@gmail.com>
2026-09-25 13:31:31 -0300

    Actualizar PWA con contacto de talleres

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lolo-real-v53';
+const CACHE='lolo-real-v54';
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg','/icon-192.svg?v=2','/icon-512.svg?v=2','/logo-lolo.svg'])));
