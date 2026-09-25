4eda42504b65c1bc4cd2b9fcbd7c29c834727640
ChrisAlonso985 <christianalonso405@gmail.com>
2026-09-24 21:30:11 -0300

    Actualizar PWA para prueba gratis por voz

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lolo-real-v49';
+const CACHE='lolo-real-v50';
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon.svg','/icon-192.svg?v=2','/icon-512.svg?v=2','/logo-lolo.svg'])));
